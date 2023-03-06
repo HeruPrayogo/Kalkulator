72e20181c018fa9d53f17d9b897bbd6b47916058
LordKajoc <rizkinuriman@upi.edu>
2023-03-06 21:45:13 +0700

    Penambahan Pengurangan

--- src/main/kotlin/Kalkulator.kt
+++ src/main/kotlin/Kalkulator.kt
@@ -8,3 +8,10 @@ class penambahan(var angka1:Int, var angka2:Int):Kalkulator(){
         return a
     }
 }
+
+class pengurangan(var angka1:Int, var angka2:Int):Kalkulator(){
+    override fun sum(): Int {
+        val a = angka1 - angka2
+        return a
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,7 @@
 fun main() {
     val kalkulator = penambahan(1, 2)
     print(kalkulator.sum())
+    println()
+    val kalkulatorpengurangan = pengurangan(1, 2)
+    print(kalkulatorpengurangan.sum())
 }
